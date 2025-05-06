ed397e99555964074507baefddad9cc8588bf792
Gorillq <kamil.kownacki3@gmail.com>
2025-05-06 10:52:56 +0200

    fixed lazyvim installation

--- lib/userrepo.rb
+++ lib/userrepo.rb
@@ -4,6 +4,8 @@ require_relative "utils"
 
 Utils.run_command("yay", "-S", "librewolf-bin", "nvim-lazy")
 #lazyvim
+Utils.run_command('nvim', '+qall')
 Utils.run_command('git', 'clone', 'https://github.com/LazyVim/starter ~/.config/nvim')
 Utils.run_command('rm', '-rf', '~/.config/nvim/.git')
+Utils.run_command('nvim', '+LazyHealth', '+qall')
 Utils.terminate
