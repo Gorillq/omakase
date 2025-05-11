a682c1d4a32f3047427eb7f6cec30fd32a7e502e
Gorillq <kamil.kownacki3@gmail.com>
2025-05-11 11:06:55 +0200

    choice to install bloatware

--- lib/userrepo.rb
+++ lib/userrepo.rb
@@ -8,4 +8,16 @@ Utils.run_command('nvim', '+qall')
 Utils.run_command('git', 'clone', 'https://github.com/LazyVim/starter ~/.config/nvim')
 Utils.run_command('rm', '-rf', '~/.config/nvim/.git')
 Utils.run_command('nvim', '+LazyHealth', '+qall')
+#Lenovo firmware
+choice = ""
+
+until ["Y", "N"].include?(choice)
+  puts "Lenovo ideapad5 firmware, install? ('Y' or 'N')"
+  choice = gets.chomp.upcase
+end
+
+if choice == "Y"
+  Utils.run_command('yay', '-S', 'ast-firmware', 'aic94xx-firmware', 'wd719x-firmware', 'linux-firmware-qlogic')
+end
+
 Utils.terminate
